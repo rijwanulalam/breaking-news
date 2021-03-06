c466238639da813e8f480cafbc740f53d428450c
unknown <rijwanul alam>
2021-03-07 05:36:14 +0600

    first commit by using material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import News from './components/News/News';
+import {React, useEffect, useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2c4cace89fae451eb6cf0424a5f4456a';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="shownews">
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
